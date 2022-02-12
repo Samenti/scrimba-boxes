4b83b9a7f349332d1c47ae6ee0b9d73339618aff
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-02-12 21:31:59 +0200

    Added boxes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import boxes from './boxes.json';
 
 export default function App() {
+  const [boxArray, setBoxArray] = React.useState(boxes);
+  
+  const boxDivs = boxArray.map(box => {
+    return (<div key={box.id} className="box" />);
+  });
+
   return (
     <div className="container">
+      {boxDivs}
     </div>
   );
 };
