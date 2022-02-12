2d04cfc7a45176b2a609247f3e16bbb897dc10fa
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2022-02-13 00:31:41 +0200

    Much more concise solution of the instructor

--- src/App.js
+++ src/App.js
@@ -7,15 +7,9 @@ export default function App(props) {
 
   function toggle(id) {
     setBoxArray(prevBoxArray => {
-      const newArray = [...prevBoxArray];
-      // *** this finds the right index of the box with 'id': ***
-      // const index = newArray.findIndex(
-      //   element => element.id == id
-      // );
-      // newArray[index].on = !newArray[index].on;
-      // *** this needs the boxes in the json array to keep order: ***
-      newArray[id-1].on = !newArray[id-1].on
-      return (newArray);
+      return prevBoxArray.map((box) => {
+        return box.id === id ? {...box, on: !box.on} : box
+      });
     });
   }
 
